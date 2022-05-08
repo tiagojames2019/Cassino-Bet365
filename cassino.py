004787d7c71ee49b77f76f2de8086c08f5e6938a
Tiago Silva <tiago.techsuport@gmail.com>
2022-05-07 22:26:19 -0300

    :rocket: versão 1.0  Analise Cassino Bet365

---
+++ cassino.py
@@ -0,0 +1,106 @@
+import os
+vermelho = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+preto = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 28, 31, 33, 35]
+
+#Grupos
+st1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+nd2 = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+rd3 = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
+
+# Grupo Horizontal
+h1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
+h2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
+h3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
+
+# Variavel contadorass
+cImpar, cPar, cCorV, cCorP, cNumSort, pCentBolasV, pCentBolasP, cSorteada, nSort = 0, 0, 0, 0, 0, 0, 0, 0, 0
+cSt1, cNd2, pImpar, pPar, cRd3, cH1, cH2, cH3, pSt1, pNd2, pRd3, pH1, pH2, pH3 = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+segue = True
+
+while segue:
+
+    print( 5 * '*', f'QTD numero analisados {cNumSort}\n' )
+    print(f'N° Sorteado : {nSort}, {cSorteada}')
+    print(35 * '_')
+    print('Impar = {} e {:.0f} %\nPar   = {} e {:.0f} %'.format(cImpar, pImpar, cPar, pPar) )
+    print(35 * '_')
+    print('VERMELHO = {} e {:.0f} % \nPRETO    = {} e {:.0f} % '.format(cCorV, pCentBolasV, cCorP, pCentBolasP))
+    print(35 * '_')
+    print('Grupo ST1 = {}  e  {:.0f} %'.format(cSt1, pSt1))
+    print('Grupo ND2 = {}  e  {:.0f} %'.format(cNd2, pNd2))
+    print('Grupo RD3 = {}  e  {:.0f} %'.format(cRd3, pRd3))
+    print(35 * '_')
+    print('Grupo H1 = {}  e  {:.0f} %'.format(cH1, pH1))
+    print('Grupo H2 = {}  e  {:.0f} %'.format(cH2, pH2))
+    print('Grupo H3 = {}  e  {:.0f} %'.format(cH3, pH3))
+    print(35 * '_')
+
+    nSort = int(input('Digite o numero Sorteado: '))
+
+    if nSort in vermelho:
+        cCorV = cCorV + 1
+        cSorteada = 'Vermelha'
+
+    elif nSort in preto:
+        cCorP =cCorP + 1
+        cSorteada = 'Preto'
+
+    # Logicas
+
+    # Quantidade de números Pares e Impares sorteados
+    if nSort % 2 == 0:
+        cPar = cPar + 1
+    else:
+        cImpar = cImpar + 1
+
+    cNumSort = cNumSort + 1
+
+    # Porcentagem de numero impar, pares
+    pCentImp = (cImpar /cNumSort)* 100
+    pCentPar = (cPar / cNumSort)* 100
+
+    # Porcentagem vermelhos e preta
+    pCentBolasV = (cCorV /cNumSort) * 100
+    pCentBolasP= (cCorP / cNumSort) * 100
+
+
+
+# Quantidades por Grupos sorteado st1, nd2, rd3
+    if nSort in st1:
+        cSt1 = cSt1 + 1
+
+    elif nSort in nd2:
+        cNd2 = cNd2 + 1
+
+    else:
+        cRd3 = cRd3 + 1
+
+    pSt1 = (cSt1 / cNumSort) * 100
+    pNd2 = (cNd2 / cNumSort) * 100
+    pRd3 = (cRd3 / cNumSort) * 100
+
+# Quantidades por Grupos sorteado h1 h2 h3
+
+    if nSort in h1:
+        cH1 = cH1 + 1
+
+    elif nSort in h2:
+        cH2 = cH2 + 1
+
+    else:
+        cH3 = cH3 + 1
+
+    pH1 = (cH1 / cNumSort) * 100
+    pH2 = (cH2 / cNumSort) * 100
+    pH3 = (cH3 / cNumSort) * 100
+    pImpar = (cImpar/cNumSort) * 100
+    pPar = (cPar / cNumSort) * 100
+
+# Troca de Sorteador
+
+# Quantos acertos 
+
+# Quantos erros
+
+#Probabilidades
+    os.system('cls')
