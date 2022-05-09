919e8932098a701ad1565a4314ddf5537ab144af
Tiago Silva <tiago.techsuport@gmail.com>
2022-05-08 22:04:24 -0300

    :rocket: inclui cotagem de Zeros e Porcentagem 	Incluso exceção de Divisão por zero 	Incluso exceção por digitações de valores invalidos

--- cassino.py
+++ cassino.py
@@ -13,94 +13,111 @@ h2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
 h3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
 
 # Variavel contadorass
-cImpar, cPar, cCorV, cCorP, cNumSort, pCentBolasV, pCentBolasP, cSorteada, nSort = 0, 0, 0, 0, 0, 0, 0, 0, 0
-cSt1, cNd2, pImpar, pPar, cRd3, cH1, cH2, cH3, pSt1, pNd2, pRd3, pH1, pH2, pH3 = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+cImpar, cPar, cCorV, cCorP, cNumSort, pCentBolasV, pCentBolasP, pCentImp, pCentPar, cSorteada, nSort, cZeros = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+cSt1, cNd2, cRd3, cH1, cH2, cH3, pSt1, pNd2, pRd3, pH1, pH2, pH3, pZeros = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 segue = True
 
 while segue:
+    try:
+        print( 5 * '*', f'QTD numero analisados {cNumSort}\n' )
+        print(f'N° Sorteado : {nSort}, {cSorteada}')
+        print(35 * '_')
+        print('Zeros = {} e {:.0f} %'.format(cZeros, pZeros))
+        print(35 * '_')
+        print('Impar = {} e {:.0f} %\nPar   = {} e {:.0f} %'.format(cImpar, pCentImp, cPar, pCentPar) )
+        print(35 * '_')
+        print('VERMELHO = {} e {:.0f} % \nPRETO    = {} e {:.0f} % '.format(cCorV, pCentBolasV, cCorP, pCentBolasP))
+        print(35 * '_')
+        print('Grupo ST1 = {}  e  {:.0f} %'.format(cSt1, pSt1))
+        print('Grupo ND2 = {}  e  {:.0f} %'.format(cNd2, pNd2))
+        print('Grupo RD3 = {}  e  {:.0f} %'.format(cRd3, pRd3))
+        print(35 * '_')
+        print('Grupo H1 = {}  e  {:.0f} %'.format(cH1, pH1))
+        print('Grupo H2 = {}  e  {:.0f} %'.format(cH2, pH2))
+        print('Grupo H3 = {}  e  {:.0f} %'.format(cH3, pH3))
+        print(35 * '_')
 
-    print( 5 * '*', f'QTD numero analisados {cNumSort}\n' )
-    print(f'N° Sorteado : {nSort}, {cSorteada}')
-    print(35 * '_')
-    print('Impar = {} e {:.0f} %\nPar   = {} e {:.0f} %'.format(cImpar, pImpar, cPar, pPar) )
-    print(35 * '_')
-    print('VERMELHO = {} e {:.0f} % \nPRETO    = {} e {:.0f} % '.format(cCorV, pCentBolasV, cCorP, pCentBolasP))
-    print(35 * '_')
-    print('Grupo ST1 = {}  e  {:.0f} %'.format(cSt1, pSt1))
-    print('Grupo ND2 = {}  e  {:.0f} %'.format(cNd2, pNd2))
-    print('Grupo RD3 = {}  e  {:.0f} %'.format(cRd3, pRd3))
-    print(35 * '_')
-    print('Grupo H1 = {}  e  {:.0f} %'.format(cH1, pH1))
-    print('Grupo H2 = {}  e  {:.0f} %'.format(cH2, pH2))
-    print('Grupo H3 = {}  e  {:.0f} %'.format(cH3, pH3))
-    print(35 * '_')
+        nSort = int(input('Digite o numero Sorteado: '))
 
-    nSort = int(input('Digite o numero Sorteado: '))
+        if nSort in vermelho:
+            cCorV = cCorV + 1
+            cSorteada = 'Vermelha'
 
-    if nSort in vermelho:
-        cCorV = cCorV + 1
-        cSorteada = 'Vermelha'
+        elif nSort in preto:
+            cCorP =cCorP + 1
+            cSorteada = 'Preto'
+        elif nSort == 0 :
+            cSorteada = 'Verde'
 
-    elif nSort in preto:
-        cCorP =cCorP + 1
-        cSorteada = 'Preto'
+        # Logicas
 
-    # Logicas
+        # Quantidade de números Pares e Impares sorteados
+        if nSort > 0 and nSort % 2 == 0 and nSort < 37 :
+            cPar = cPar + 1
+            cNumSort = cNumSort + 1
+        elif nSort > 0 and nSort % 2 > 0 and nSort < 37:
+            cImpar = cImpar + 1
+            cNumSort = cNumSort + 1
+        elif nSort == 0 :
+            cZeros = cZeros +1
+            cNumSort = cNumSort + 1
 
-    # Quantidade de números Pares e Impares sorteados
-    if nSort % 2 == 0:
-        cPar = cPar + 1
-    else:
-        cImpar = cImpar + 1
+    # Quantidades por Grupos sorteado st1, nd2, rd3
+        if nSort in st1:
+            cSt1 = cSt1 + 1
 
-    cNumSort = cNumSort + 1
+        elif nSort in nd2:
+            cNd2 = cNd2 + 1
 
-    # Porcentagem de numero impar, pares
-    pCentImp = (cImpar /cNumSort)* 100
-    pCentPar = (cPar / cNumSort)* 100
+        elif nSort in rd3:
+            cRd3 = cRd3 + 1
 
-    # Porcentagem vermelhos e preta
-    pCentBolasV = (cCorV /cNumSort) * 100
-    pCentBolasP= (cCorP / cNumSort) * 100
+        pSt1 = (cSt1 / cNumSort) * 100
+        pNd2 = (cNd2 / cNumSort) * 100
+        pRd3 = (cRd3 / cNumSort) * 100
+
+    # Quantidades por Grupos sorteado h1 h2 h3
+
+        if nSort in h1:
+            cH1 = cH1 + 1
+
+        elif nSort in h2:
+            cH2 = cH2 + 1
+
+        elif nSort in h3:
+            cH3 = cH3 + 1
+
+        # Porcentagem de numero impar, pares
+        pCentImp = (cImpar / cNumSort) * 100
+        pCentPar = (cPar / cNumSort) * 100
+
+        # Porcentagem vermelhos e preta
+        pCentBolasV = (cCorV / cNumSort) * 100
+        pCentBolasP = (cCorP / cNumSort) * 100
+
+        pH1 = (cH1 / cNumSort) * 100
+        pH2 = (cH2 / cNumSort) * 100
+        pH3 = (cH3 / cNumSort) * 10
+
+        pZeros = (cZeros / cNumSort) * 100
+
+        if nSort > 36:
+            cSorteada = 0
+            print(5 * '*', 'ATENÇÃO', 5 * '*')
+            print("Numero digitado Maior que permitido")
 
 
+    # Troca de Sorteador
 
-# Quantidades por Grupos sorteado st1, nd2, rd3
-    if nSort in st1:
-        cSt1 = cSt1 + 1
+    # Quantos acertos
 
-    elif nSort in nd2:
-        cNd2 = cNd2 + 1
+    # Quantos erros
 
-    else:
-        cRd3 = cRd3 + 1
+    #Probabilidades
+    except ZeroDivisionError:
+        print(5 * '*', 'ATENÇÃO', 5 * '*')
+        print('O numero inicial digitado é maior que o permitido ')
+    except ValueError:
+        print('Tipo de Valor invalido')
 
-    pSt1 = (cSt1 / cNumSort) * 100
-    pNd2 = (cNd2 / cNumSort) * 100
-    pRd3 = (cRd3 / cNumSort) * 100
-
-# Quantidades por Grupos sorteado h1 h2 h3
-
-    if nSort in h1:
-        cH1 = cH1 + 1
-
-    elif nSort in h2:
-        cH2 = cH2 + 1
-
-    else:
-        cH3 = cH3 + 1
-
-    pH1 = (cH1 / cNumSort) * 100
-    pH2 = (cH2 / cNumSort) * 100
-    pH3 = (cH3 / cNumSort) * 100
-    pImpar = (cImpar/cNumSort) * 100
-    pPar = (cPar / cNumSort) * 100
-
-# Troca de Sorteador
-
-# Quantos acertos 
-
-# Quantos erros
-
-#Probabilidades
     os.system('cls')
